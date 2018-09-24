b58630f414d9044f2052afc953cdefd8c82e0542
ryannamgung <ryannamgung@gmail.com>
2018-09-24 10:07:06 -0400

    boilerplate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function reducer(state, action) {
+  switch(action.type){
+    default:
+      return state
+  }
+  return state;
+}
+
+const store = createStore(reducer)
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+//App
+//UploadForm
+//CompanyList
+//Company
+//Company Profile
